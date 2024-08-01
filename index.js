c97a309e2f669e32ce8017ef36385ff6457a46a1
Du.Kang <banshee1115@163.com>
2024-08-01 23:49:05 +0800

    基础类型

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * @Author: Du.Kang banshee1115@163.com
+ * @Date: 2024-08-01 23:09:12
+ * @LastEditors: Du.Kang banshee1115@163.com
+ * @LastEditTime: 2024-08-01 23:45:40
+ * @FilePath: /typescript-learning/index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+let str = 'dk';
+let num = 1;
